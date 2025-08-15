83e51ff8780b47fc1adc168d6d137805828cedd2
shahidulllah <mdshahidsumon177@gmail.com>
2025-08-15 20:53:46 +0600

    testing

--- src/components/countries/PhotoCard.jsx
+++ src/components/countries/PhotoCard.jsx
@@ -5,6 +5,7 @@ const PhotoCard = ({photo}) => {
         <div>
             <img src={photo.thumbnailUrl} alt={photo.thumbnailUrl} />
             <h1>{photo.title}</h1>
+            <h2>{photo.thumbnailUrl}</h2>
         </div>
     );
 };
