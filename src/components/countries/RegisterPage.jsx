772f9db25633919be50cc4981cdd756e9291dd56
shahidulllah <mdshahidsumon177@gmail.com>
2025-08-20 00:43:07 +0600

    modify register

--- src/components/countries/RegisterPage.jsx
+++ src/components/countries/RegisterPage.jsx
@@ -1,8 +1,44 @@
 const RegisterPage = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-      <label>Name</label>
-      <input type="text" name="name" placeholder="Write your name" />
+    <div className="hero min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse max-w-5xl">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Create an account!</h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+        </div>
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <div className="card-body">
+            <form onSubmit={handleSubmit}>
+              <fieldset className="fieldset">
+                <label className="label">Name</label>
+                <input type="text" className="input" name="name" placeholder="Email" />
+                <label className="label">Email</label>
+                <input type="email" name="email" className="input" placeholder="Email" />
+                <label className="label">Password</label>
+                <input
+                  type="password"
+                  className="input"
+                  placeholder="Password"
+                  name="password"
+                />
+                <div>
+                  <a className="link link-hover">Forgot password?</a>
+                </div>
+                <button type="submit" className="btn btn-neutral mt-4">
+                  Login
+                </button>
+              </fieldset>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
