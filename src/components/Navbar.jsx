a55d6e2fc5c866901d9b4350f166ca9626358050
shahidulllah <mdshahidsumon177@gmail.com>
2025-08-21 11:59:54 +0600

    modify nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,10 @@
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
-  const navlinks = <></>;
+  const navlinks = <div className="space-x-2">
+  <NavLink to={'/'}>Home</NavLink>
+  <NavLink to={'/photo'}>Galleries</NavLink>
+  </div>;
   return (
     <div className="bg-base-100 shadow-sm">
       <div className="navbar max-w-7xl mx-auto">
@@ -8,6 +13,7 @@ const Navbar = () => {
         </div>
         <div className="flex-none">
           <ul className="menu menu-horizontal px-1">{navlinks}</ul>
+          
         </div>
       </div>
     </div>
