9364f0b50787c925fedc305e55e7a949d7faec25
shahidulllah <mdshahidsumon177@gmail.com>
2025-08-28 19:17:25 +0600

    working on main layout

--- src/MainLayout.jsx
+++ src/MainLayout.jsx
@@ -1,14 +1,22 @@
 import Navbar from "./components/Navbar";
 
-
 function App() {
   return (
     <>
       <div>
-        <Navbar/>
-        <h1 className="font-bold text-4xl text-center mt-7">
-          React Concepts Practice
-        </h1>
+        <Navbar />
+        <div className=" max-w-7xl mx-auto">
+          <h1 className="font-bold text-4xl text-center my-5">
+            React Concepts Practice
+          </h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veniam in
+            possimus quis, magni illo repudiandae voluptatum veritatis
+            molestiae. Nisi non, quisquam mollitia dolorem cupiditate distinctio
+            aspernatur similique ad optio sint consequatur ut nostrum dolorum
+            nesciunt.
+          </p>
+        </div>
       </div>
     </>
   );
