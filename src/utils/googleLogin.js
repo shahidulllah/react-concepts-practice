7bc66de4a4150f93922f4b4bff8bb3f74122cf80
shahidulllah <mdshahidsumon177@gmail.com>
2025-08-29 19:49:55 +0600

    google login sestem

--- src/utils/googleLogin.js
+++ src/utils/googleLogin.js
@@ -1,3 +1,14 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../firebase/firebase.config";
+
+const goggleProvider = GoogleAuthProvider();
+
 export function googleLogin() {
-    
+  signInWithPopup(auth, goggleProvider)
+    .then((result) => {
+      console.log(result.user);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
