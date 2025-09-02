e822a4f6438e02519c7685809cda54557101c7da
shahidulllah <mdshahidsumon177@gmail.com>
2025-09-02 09:53:52 +0600

    geting user using hook

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,8 @@
-import { useContext } from "react";
 import { NavLink } from "react-router-dom";
-import { AuthContext } from "../providers/AuthProvider";
+import useAuth from "../hooks/useAuth";
 
 const Navbar = () => {
-  const { user } = useContext(AuthContext);
-  console.log(user.email);
+  const { user } = useAuth();
 
   const navlinks = (
     <div className="space-x-4">
