bd0417948cd1ac53d4f361d1c894a14c3d389051
shahidulllah <mdshahidsumon177@gmail.com>
2025-09-09 22:26:49 +0600

    refector code

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,11 +1,10 @@
-
 const ErrorPage = () => {
-    return (
-        <div className="h-screen flex flex-col items-center justify-center space-y-4">
-           <h1 className="font-bold text-6xl">Not found</h1> 
-           <p className="text-xl font-medium">Something is wrong..!</p>
-        </div>
-    );
+  return (
+    <div className="h-screen flex flex-col items-center justify-center space-y-4">
+      <h1 className="font-bold text-6xl">Not found</h1>
+      <p className="text-xl font-medium">Something is wrong..!</p>
+    </div>
+  );
 };
 
 export default ErrorPage;
