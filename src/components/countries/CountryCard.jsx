5e7e8e5f92cd37903797a4b9b8a4ed6cf8bbffbc
shahidulllah <mdshahidsumon177@gmail.com>
2025-09-11 19:14:26 +0600

    working

--- src/components/countries/CountryCard.jsx
+++ src/components/countries/CountryCard.jsx
@@ -1,7 +1,12 @@
 const CountryCard = () => {
   return (
     <div>
-      <h1>This is coutry card</h1>
+      <h1>This is country card</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus
+        facere animi velit optio odit laboriosam, nobis delectus temporibus
+        distinctio magni aliquam deleniti ex!
+      </p>
     </div>
   );
 };
