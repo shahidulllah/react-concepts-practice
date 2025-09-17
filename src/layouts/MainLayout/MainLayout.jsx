209ec91244c7c4e33b48068b02033facc11ccc3f
shahidulllah <mdshahidsumon177@gmail.com>
2025-09-17 21:40:01 +0600

    footer added to the main layout

--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../../components/Navbar";
 import Title from "../../shared/Title";
+import Footer from "../../pages/home/Footer";
 
 function MainLayout() {
   return (
     <>
       <div>
         <Navbar />
-        <Outlet/>
+        <Outlet />
+        <Footer />
       </div>
     </>
   );
