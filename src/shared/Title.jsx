dc6c290935dc8667cadce553bf1924f51d5e77ff
shahidulllah <mdshahidsumon177@gmail.com>
2025-09-29 20:35:41 +0600

    just working

--- src/shared/Title.jsx
+++ src/shared/Title.jsx
@@ -1,7 +1,7 @@
 const Title = ({ title, description }) => {
   return (
     <div className="text-center py-4">
-      <h1 className="text-4xl">{title}</h1>
+      <h1 className="text-4xl font-semibold">{title}</h1>
       <p>{description}</p>
     </div>
   );
