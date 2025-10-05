e8c64471943ac967464919e978d4b5ab78c24107
shahidulllah <mdshahidsumon177@gmail.com>
2025-10-05 10:07:34 +0600

    counter is working

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,17 +1,34 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  decrement,
+  increment,
+  incrementByValue,
+} from "../../redux/features/slices/counterSlice";
 
 const Counter = () => {
-    const count = useSelector((state)=> state.counterReducer)
-    console.log(count);
-    return (
-        <div>
-            <button>Decrement</button>
-            <div></div>
-            <button>Increment</button>
-            <button>Increment 5</button>
-        </div>
-    );
+  const { value } = useSelector((state) => state.counterReducer);
+  const dispatch = useDispatch();
+  return (
+    <div className="h-44 text-center mt-16">
+      <button onClick={() => dispatch(decrement())} className="btn btn-accent">
+        Decrement
+      </button>
+      <div className="my-5 text-2xl font-semibold">{value}</div>
+      <button
+        onClick={() => dispatch(increment())}
+        className="btn btn-accent mr-12"
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => dispatch(incrementByValue(5))}
+        className="btn btn-accent"
+      >
+        Increment 5
+      </button>
+    </div>
+  );
 };
 
 export default Counter;
