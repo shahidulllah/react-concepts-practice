1b02092caa0e0be27ec7352db4f46b59d4cc3282
shahidulllah <mdshahidsumon177@gmail.com>
2025-10-07 12:07:11 +0600

    trying to use RTK query

---
+++ src/redux/features/baseApi/baseApi.js
@@ -0,0 +1,11 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const baseApi = createApi({
+  reducerPath: "baseApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "" }),
+  endpoints: (builder) => ({
+    getPosts: builder.query({
+      query: () => "",
+    }),
+  }),
+});
