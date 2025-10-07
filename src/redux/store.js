58548c0cf068c8d223cd22fdac95fc5d5bb01eed
shahidulllah <mdshahidsumon177@gmail.com>
2025-10-07 12:12:37 +0600

    connect base api to the store and added middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { counterReducer } from "./features/slices/counterSlice";
+import { baseApi } from "./features/baseApi/baseApi";
 
 export const store = configureStore({
   reducer: {
     counterReducer,
+    [baseApi.reducerPath]: baseApi.reducer,
   },
+  middleware: (getDefaultMiddleware)=> {
+    getDefaultMiddleware().concat(baseApi.middleware)
+  }
 });
