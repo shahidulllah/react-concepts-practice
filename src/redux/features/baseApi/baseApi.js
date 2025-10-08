ec647ff6d6e9c5217d24a8492ba1d271b3af483b
shahidulllah <mdshahidsumon177@gmail.com>
2025-10-08 09:49:51 +0600

    cachign

--- src/redux/features/baseApi/baseApi.js
+++ src/redux/features/baseApi/baseApi.js
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
+  tagTypes: "Counter",
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({ baseUrl: "" }),
   endpoints: (builder) => ({
@@ -13,6 +14,7 @@ export const baseApi = createApi({
         method: "POST",
         body: "",
       }),
+      invalidatesTags: "Counter",
     }),
   }),
 });
