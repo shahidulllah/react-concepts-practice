21b1eee2967572d16bca1538d20e2e4cbad7ac2d
shahidulllah <mdshahidsumon177@gmail.com>
2025-10-12 20:41:50 +0600

    css adding

--- src/shared/Title.jsx
+++ src/shared/Title.jsx
@@ -1,8 +1,8 @@
 const Title = ({ title, description }) => {
   return (
     <div className="text-center py-4">
-      <h1 className="text-4xl font-semibold">{title}</h1>
-      <p>{description}</p>
+      <h1 className="text-4xl font-semibold py-8 text-center">{title}</h1>
+      <p className="text-justify">{description}</p>
     </div>
   );
 };
